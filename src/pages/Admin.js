08a1ff2d02c0969f2f302f8904e08e1e8cca4818
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-04-25 20:40:03 +0200

    ModalDevice and Admin page update

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
 
-import { ModalType } from 'components/ModalType/ModalType';
 import { ModalBrand } from 'components/ModalBrand/ModalBrand';
+import { ModalDevice } from 'components/ModalDevice/ModalDevice';
+import { ModalType } from 'components/ModalType/ModalType';
 
 export const Admin = () => {
+  const [brandVisible, setBrandVisible] = useState(false);
+  const [deviceVisible, setDeviceVisible] = useState(false);
+  const [typeVisible, setTypeVisible] = useState(false);
+
   return (
     <Container className="d-flex flex-column">
-      <Button variant={'outline-dark'} className="mt-4 p-2">
-        Add type
-      </Button>
-      <Button variant={'outline-dark'} className="mt-4 p-2">
+      <Button
+        variant={'outline-dark'}
+        className="mt-4 p-2"
+        onClick={() => setBrandVisible(true)}
+      >
         Add brand
       </Button>
-      <Button variant={'outline-dark'} className="mt-4 p-2">
+      <Button
+        variant={'outline-dark'}
+        className="mt-4 p-2"
+        onClick={() => setDeviceVisible(true)}
+      >
         Add device
       </Button>
-      <ModalBrand show={false} />
-      <ModalType show={false} />
-      {/* <ModalDevice/> */}
+      <Button
+        variant={'outline-dark'}
+        className="mt-4 p-2"
+        onClick={() => setTypeVisible(true)}
+      >
+        Add type
+      </Button>
+      <ModalBrand show={brandVisible} onHide={() => setBrandVisible(false)} />
+      <ModalDevice
+        show={deviceVisible}
+        onHide={() => setDeviceVisible(false)}
+      />
+      <ModalType show={typeVisible} onHide={() => setTypeVisible(false)} />
     </Container>
   );
 };
