cdf8e8945decc68330b93bc46f845f10d1753624
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-04-29 20:09:06 +0200

    ModalType update: new types add is possible now

--- src/components/ModalType/ModalType.jsx
+++ src/components/ModalType/ModalType.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
 
+import { createType } from 'http/deviceAPI';
+
 export const ModalType = ({ show, onHide }) => {
+  const [value, setValue] = useState('');
+
+  const addType = () => {
+    createType({ name: value }).then(data => {
+      setValue('');
+      onHide();
+    });
+  };
+
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
       <Modal.Header closeButton>
@@ -11,14 +22,18 @@ export const ModalType = ({ show, onHide }) => {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Control placeholder={'Enter the name of new Type'} />
+          <Form.Control
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            placeholder={'Enter the name of new Type'}
+          />
         </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="outline-danger" onClick={onHide}>
           Close
         </Button>
-        <Button variant="outline-success" onClick={onHide}>
+        <Button variant="outline-success" onClick={addType}>
           Add
         </Button>
       </Modal.Footer>
